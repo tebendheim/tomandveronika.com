025f6bd4dbbef57fbc7e34d9e07f99554e6c6689
tebendheim <tomelbin94@gmail.com>
2022-07-22 18:56:23 +0200

    FFÃorsøker endret forgotpassword... cors error??

--- src/Pages/Forgotpassword.js
+++ src/Pages/Forgotpassword.js
@@ -26,7 +26,7 @@ function Forgotpassword(props) {
 		const testurl = 'http://localhost:8888/api/forgotpassword';
 		const params = {
 			headers: {
-				'content-type': 'text/plain',
+				'content-type': 'application/JSON',
 				'Access-Control-Allow-Origin': '*',
 			},
 			data: {
