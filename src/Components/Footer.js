53b1c842bdacd754e2aaab6e16bbf2b6954d0fd1
Tom-Elbin Bendheim <tom-elbinbendheim@Tom-Elbins-MacBook-Pro.local>
2023-02-24 19:07:04 +0100

    endret navbar, samt at footer er nederst på home

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,17 +17,17 @@ const Footer = () => {
 			<ul className={style.menuObjects}>
 				<li className={style.navElement}>
 					<a onClick={falseClick} className={style.atag} href='https://www.facebook.com/Tom-Elbin-Bendheim-493293257858119'>
-						<i class="fa fa-facebook-square" aria-hidden="true"></i>
+						<i className="fa fa-facebook-square" aria-hidden="true"></i>
 					</a>
 				</li>
 				<li className={style.navElement}>
 					<a onClick={falseClick} className={style.atag} href='/contact'>
-						<i class="fa fa-envelope" aria-hidden="true"></i>
+						<i className="fa fa-envelope" aria-hidden="true"></i>
 					</a>
 				</li>
 				<li className={style.navElement}>
 					<a onClick={falseClick} className={style.atag} href='https://www.instagram.com/veronikalangmo'>
-						<i class="fa fa-instagram" aria-hidden="true"></i>
+						<i className="fa fa-instagram" aria-hidden="true"></i>
 					</a>
 				</li>
 			</ul>
